03d35cab2365539648df8c9a24e7f35e9d9889e5
Haoran Feng <haoran.feng@sap.com>
2015-06-24 10:46:45 +0800

    Fix a deprecated API.

--- src/test/java/org/hfeng/leet/rotateimage/SolutionTest.java
+++ src/test/java/org/hfeng/leet/rotateimage/SolutionTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
@@ -23,6 +24,6 @@ public class SolutionTest {
         int[][] m1 = new int[][] { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] r1 = new int[][] { {7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
         solution.rotate(m1);
-        assertEquals(r1, m1);
+        assertArrayEquals(r1, m1);
     }
 }
