b9f6344d2d471347b40a9de407d8d2926cf443f2
Haoran Feng <haoran.feng@sap.com>
2015-07-24 13:50:38 +0800

    Add new leet, tree problem use recursive solution intensively

--- src/main/java/org/hfeng/leet/symmetrictree/Solution.java
+++ src/main/java/org/hfeng/leet/symmetrictree/Solution.java
@@ -2,55 +2,23 @@ package org.hfeng.leet.symmetrictree;
 
 import org.hfeng.leet.util.TreeNode;
 
-import java.util.*;
-
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return true;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<Integer> ret = new ArrayList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            TreeNode head = queue.poll();
-            if (head == null) {
-                ret.add(-1);
-                continue;
-            } else {
-                ret.add(head.val);
-            }
-            queue.add(head.left);
-            queue.add(head.right);
-
-        }
-        int len = Integer.highestOneBit(ret.size());
-        List<Integer> tt = ret.subList(0, len - 1);
-        return isOK(tt);
+        return isSym(root.left, root.right);
     }
 
-    private boolean isOK(List<Integer> list) {
-        if (list.size() == 1) {
+    private boolean isSym(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
             return true;
         }
-        int mid = list.size() / 2;
-        return isOK(list.subList(0, mid)) && isSym(list.subList(mid, list.size()));
-    }
-
-    private boolean isSym(List<Integer> list) {
-        if (list.size() % 2 == 1) {
+        if (left == null || right == null) {
             return false;
         }
-        int beg = 0;
-        int end = list.size() - 1;
-        while (beg < end) {
-            if (list.get(beg) != list.get(end)) {
-                return false;
-            }
-            beg++;
-            end--;
-        }
-        return true;
+        return left.val == right.val
+                && isSym(left.left, right.right)
+                && isSym(left.right, right.left);
     }
-
 }
