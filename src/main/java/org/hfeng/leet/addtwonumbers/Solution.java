2b03d185b94d0d9e7da0bee6443cf3d81ed6f74f
Haoran Feng <haoran.feng@sap.com>
2015-08-21 20:55:42 +0800

    1st

--- src/main/java/org/hfeng/leet/addtwonumbers/Solution.java
+++ src/main/java/org/hfeng/leet/addtwonumbers/Solution.java
@@ -6,20 +6,19 @@ public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(-1);
         ListNode tmp = head;
-        int carry = 0;
+        int adv = 0;
         while (l1 != null || l2 != null) {
-            int v1 = (l1 != null) ? l1.val : 0;
-            int v2 = (l2 != null) ? l2.val : 0;
-
-            int sum = v1 + v2 + carry;
+            int v1 = (l1 == null) ? 0 : l1.val;
+            int v2 = (l2 == null) ? 0 : l2.val;
+            int sum = v1 + v2 + adv;
+            adv = sum / 10;
             head.next = new ListNode(sum % 10);
             head = head.next;
-
-            carry = sum / 10;
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
+            l1 = (l1 == null) ? null : l1.next;
+            l2 = (l2 == null) ? null : l2.next;
         }
-        if (carry > 0) {
+
+        if (adv == 1) {
             head.next = new ListNode(1);
         }
         return tmp.next;
