fb7da9dae2309c8df0844d2f70961a3dde45f067
Haoran Feng <haoran.feng@sap.com>
2015-08-21 06:47:38 -0700

    3rd

--- src/main/java/org/hfeng/leet/atoi/Solution.java
+++ src/main/java/org/hfeng/leet/atoi/Solution.java
@@ -1,29 +1,32 @@
 package org.hfeng.leet.atoi;
+
 public class Solution {
     public int atoi(String str) {
-        if (str.length() == 0 || str == null) {
+        if (str.length() == 0) {
             return 0;
         }
-        int start = 0;
-        while (str.charAt(start) == ' ') {
-            start++;
-        }
+        int beg = 0;
         boolean neg = false;
-        if (str.charAt(start) == '-') {
-            neg = true;
-            start++;
-        } else if (str.charAt(start) == '+') {
-            neg = false;
-            start++;
+
+        while (str.charAt(beg) == ' ') {
+            beg++;
         }
+
+        if (str.charAt(beg) == '-') {
+            neg = true;
+            beg++;
+        } else if (str.charAt(beg) == '+') {
+            neg = false;
+            beg++;
+        }
+
         int ret = 0;
 
-        for (int i = start; i < str.length(); i++) {
+        for (int i = beg; i < str.length(); i++) {
             if (Character.isDigit(str.charAt(i))) {
                 int cur = Character.digit(str.charAt(i), 10);
-                if (ret > Integer.MAX_VALUE / 10
-                    || (ret == Integer.MAX_VALUE / 10
-                        && cur > Integer.MAX_VALUE % 10)) {
+                if (ret > Integer.MAX_VALUE / 10 ||
+                        (ret == Integer.MAX_VALUE / 10 && cur > Integer.MAX_VALUE % 10)) {
                     return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
                 } else {
                     ret = ret * 10 + cur;
@@ -32,6 +35,20 @@ public class Solution {
                 break;
             }
         }
+/*
+        for (; beg < str.length(); beg++) {
+            if (Character.isDigit(str.charAt(beg))) {
+                if (ret > Integer.MAX_VALUE / 10
+                        || (ret == Integer.MAX_VALUE / 10 && Character.digit(str.charAt(beg), 10) > Integer.MAX_VALUE % 10)) {
+                    return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                } else {
+                    ret += ret * 10 + Character.digit(str.charAt(beg), 10);
+                }
+            } else {
+                break;
+            }
+        }
+*/
         return neg ? (-1) * ret : ret;
     }
 }
