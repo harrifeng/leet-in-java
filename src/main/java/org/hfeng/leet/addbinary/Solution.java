88522663f4d225a611fdb2f5b9b9deba677e853a
Haoran Feng <haoran.feng@sap.com>
2015-08-21 05:26:50 -0700

    0th

--- src/main/java/org/hfeng/leet/addbinary/Solution.java
+++ src/main/java/org/hfeng/leet/addbinary/Solution.java
@@ -1,17 +1,21 @@
 package org.hfeng.leet.addbinary;
+
 public class Solution {
     public String addBinary(String a, String b) {
+
         StringBuilder sb = new StringBuilder();
         int adv = 0;
-        for (int i = 0; i < a.length() || i < b.length(); i++) {
-            char cha = a.length() > i ? a.charAt(a.length() - i - 1) : '0';
-            char chb = b.length() > i ? b.charAt(b.length() - i - 1) : '0';
-            int sum = adv + (cha - '0') + (chb - '0');
-            sb.insert(0, (sum % 2)) ;
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            int an = (i >= 0) ? Character.digit(a.charAt(i), 10) : 0;
+            int bn = (j >= 0) ? Character.digit(b.charAt(j), 10) : 0;
+            int sum = an + bn + adv;
+
+            sb.insert(0, sum % 2);
             adv = sum / 2;
+
         }
-        if (adv != 0) {
-            sb.insert(0, '1');
+        if (adv == 1) {
+            sb.insert(0, "1");
         }
         return sb.toString();
     }
