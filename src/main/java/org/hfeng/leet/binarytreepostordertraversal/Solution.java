6f28d980efdc42364074de34fda7d596db52453a
Haoran Feng <haoran.feng@sap.com>
2015-08-22 00:57:25 -0700

    8

--- src/main/java/org/hfeng/leet/binarytreepostordertraversal/Solution.java
+++ src/main/java/org/hfeng/leet/binarytreepostordertraversal/Solution.java
@@ -2,36 +2,27 @@ package org.hfeng.leet.binarytreepostordertraversal;
 
 import java.util.*;
 
-import org.hfeng.leet.util.TreeNode;
+import org.hfeng.leet.util.*;
 
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ret = new ArrayList<Integer>();
+        List<Integer> retV = new ArrayList<>();
+        post(root, retV);
+        return retV;
+    }
+
+    private void post(TreeNode root, List<Integer> ret) {
         if (root == null) {
-            return ret;
+            return;
+        }
+        if (root.left != null) {
+            post(root.left, ret);
         }
 
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-
-        TreeNode tmp = root;
-        stack.push(tmp);
-        TreeNode pre = null;
-
-        while (!stack.empty()) {
-            if ((pre == null || pre.left == stack.peek() || pre.right == stack.peek())
-                    && stack.peek().left != null) {
-                pre = stack.peek();
-                stack.push(stack.peek().left);
-            } else if (((stack.peek().left == null && pre != stack.peek().right) || pre == stack.peek().left)
-                    && stack.peek().right != null) {
-                pre = stack.peek();
-                stack.push(stack.peek().right);
-            } else {
-                pre = stack.peek();
-                stack.pop();
-                ret.add(pre.val);
-            }
+        if (root.right != null) {
+            post(root.right, ret);
         }
-        return ret;
+
+        ret.add(root.val);
     }
 }
