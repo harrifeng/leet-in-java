41ccf2c040b3ea243df06a7d3d62b47fd5ecfd41
Haoran Feng <haoran.feng@sap.com>
2015-08-25 02:00:15 -0700

    14th

--- src/main/java/org/hfeng/leet/countandsay/Solution.java
+++ src/main/java/org/hfeng/leet/countandsay/Solution.java
@@ -1,23 +1,27 @@
 package org.hfeng.leet.countandsay;
-
 public class Solution {
     public String countAndSay(int n) {
-        StringBuilder s = new StringBuilder("1");
 
-        for (int i = 1; i < n; i++) {
+        StringBuilder ret = new StringBuilder("1");
+        if (n == 1) {
+            return ret.toString();
+        }
+
+        for (int i = 2; i <= n; i++) {
             StringBuilder tmp = new StringBuilder();
-            int cnt = 1;
-            for (int j = 1; j < s.length(); j++) {
-                if (s.charAt(j) == s.charAt(j - 1)) {
+            int cnt = 0;
+            for (int j = 0; j < ret.length(); j++) {
+                if (j == 0 || ret.charAt(j) == ret.charAt(j-1)) {
                     cnt++;
                 } else {
-                    tmp.append(cnt).append(s.charAt(j - 1));
-                    cnt = 1;
+                    tmp.append(cnt).append(ret.charAt(j-1));
+                    cnt= 1;
                 }
             }
-            tmp.append(cnt).append(s.charAt(s.length() - 1));
-            s = tmp;
+            tmp.append(cnt).append(ret.charAt(ret.length() - 1));
+            ret = tmp;
         }
-        return s.toString();
+
+        return ret.toString();
     }
 }
