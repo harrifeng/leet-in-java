4dc2bdf596daeeeeb57bc8a63f84ef4b7177e060
Feng <haoran.feng@sap.com>
2015-09-23 21:30:18 -0700

    new style

--- src/main/java/org/hfeng/leet/evaluatereversepolishnotation/Solution.java
+++ src/main/java/org/hfeng/leet/evaluatereversepolishnotation/Solution.java
@@ -1,41 +1,36 @@
 package org.hfeng.leet.evaluatereversepolishnotation;
 
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.*;
 
 public class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<Integer>();
-        ArrayList<String> opList = new ArrayList<String>();
-        opList.add("+");
-        opList.add("-");
-        opList.add("*");
-        opList.add("/");
-
         for (int i = 0; i < tokens.length; i++) {
-            int opt = opList.indexOf(tokens[i]);
-            if (opt > -1) {
-                //operator
-                int right = stack.pop();
-                int left = stack.pop();
-                switch (opt) {
-                    case 0:
-                        stack.push(left + right);
-                        break;
-                    case 1:
-                        stack.push(left - right);
-                        break;
-                    case 2:
-                        stack.push(left * right);
-                        break;
-                    case 3:
-                        stack.push(left / right);
-                        break;
-                }
-            } else {
+            char now = tokens[i].charAt(0);
+            if (Character.isDigit(now)) {
+                //stack.push(Character.digit(now, 10));
                 stack.push(Integer.parseInt(tokens[i]));
+            } else {
+                int two = stack.pop();
+                int one = stack.pop();
+                switch (now) {
+                case '+':
+                    stack.push(one + two);
+                    break;
+                case '-':
+                    stack.push(one - two);
+                    break;
+                case '*':
+                    stack.push(one * two);
+                    break;
+                case '/':
+                    stack.push(one / two);
+                    break;
+                default:
+                    break;
+                }
             }
         }
-        return stack.peek();
+        return stack.pop();
     }
 }
