51bd52b577ea2c82d947efc82ec4daf491204dc4
Harri Feng <harrifeng@gmail.com>
2015-10-22 12:30:02 +0000

    17, the index+1 or not, and j from i + 1

--- src/main/java/org/hfeng/leet/foursum/Solution.java
+++ src/main/java/org/hfeng/leet/foursum/Solution.java
@@ -1,36 +1,41 @@
 package org.hfeng.leet.foursum;
-import java.util.*;
 
+import java.util.*;
 public class Solution {
     public List<List<Integer>> fourSum(int[] num, int target) {
-        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (num.length < 4) {
+            return null;
+        }
         Arrays.sort(num);
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
 
         for (int i = 0; i < num.length - 3; i++) {
-            if (i > 0 && num[i] == num[i - 1]) {
+            if (i > 0 && num[i-1] == num[i]) {
                 continue;
             }
             for (int j = i + 1; j < num.length - 2; j++) {
-                if (j > i + 1 && num[j] == num[j - 1]) {
+                if (j > i + 1&& num[j-1] == num[j]) {
                     continue;
                 }
+
                 int beg = j + 1;
                 int end = num.length - 1;
+
                 while (beg < end) {
                     int sum = num[i] + num[j] + num[beg] + num[end];
+
                     if (sum == target) {
-                        List<Integer> one = new ArrayList<Integer>();
-                        one.add(num[i]);
-                        one.add(num[j]);
-                        one.add(num[beg]);
-                        one.add(num[end]);
-                        ret.add(one);
-                        while (beg < end
-                               && num[beg] == num[beg+1]
+                        List<Integer> oneRet = new ArrayList<Integer>();
+                        oneRet.add(num[i]);
+                        oneRet.add(num[j]);
+                        oneRet.add(num[beg]);
+                        oneRet.add(num[end]);
+                        while (beg < end && num[beg+1] == num[beg]
                                && num[end] == num[end-1]) {
                             beg++;
                             end--;
                         }
+                        ret.add(oneRet);
                         beg++;
                         end--;
                     } else if (sum < target) {
