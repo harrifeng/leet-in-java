654aa92945bf8f4dd8fa1cb39c12701be8591259
i309511 <harrifeng@gmail.com>
2015-12-02 23:58:03 -0800

    23

--- src/main/java/org/hfeng/leet/linkedlistcycle/Solution.java
+++ src/main/java/org/hfeng/leet/linkedlistcycle/Solution.java
@@ -1,18 +1,24 @@
 package org.hfeng.leet.linkedlistcycle;
-import org.hfeng.leet.util.ListNode;
+
+import org.hfeng.leet.util.*;
 
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
         ListNode fast = head;
         ListNode slow = head;
 
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+
             if (slow == fast) {
                 return true;
             }
         }
+
         return false;
     }
 }
