315119fb805ac0eac73eb56ac8499086d9e70aba
i309511 <harrifeng@gmail.com>
2015-12-12 17:52:42 -0800

    27

--- src/main/java/org/hfeng/leet/longestsubstringwithoutrepeat/Solution.java
+++ src/main/java/org/hfeng/leet/longestsubstringwithoutrepeat/Solution.java
@@ -1,24 +1,30 @@
 package org.hfeng.leet.longestsubstringwithoutrepeat;
 
 import java.util.*;
-
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0 || s == null) {
+            return 0;
+        }
+
         int[] index = new int[256];
         Arrays.fill(index, -1);
+        int maxValue = 0;
+        int size = 0;
 
-        int len = 0;
-        int maxLen = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (index[s.charAt(i)] != -1) {
-                maxLen = Math.max(maxLen, len);
-                i = index[s.charAt(i)] + 1;
+            // Take careful when using local variable,
+            // as the i is changed later
+            char cur = s.charAt(i);
+            if (index[cur] != -1) {
+                maxValue = Math.max(maxValue, size);
+                size = 0;
+                i = index[cur] + 1;
                 Arrays.fill(index, -1);
-                len = 0;
             }
+            ++size;
             index[s.charAt(i)] = i;
-            len++;
         }
-        return Math.max(maxLen, len);
+        return Math.max(maxValue, size);
     }
 }
