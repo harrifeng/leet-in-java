b589283b850aa3c423a07f2b31583c3e9207d53c
i309511 <harrifeng@gmail.com>
2015-12-12 17:37:08 -0800

    not very good-looking, but pass the test 26

--- src/main/java/org/hfeng/leet/longestpalindromicsubstr/Solution.java
+++ src/main/java/org/hfeng/leet/longestpalindromicsubstr/Solution.java
@@ -1,58 +1,57 @@
 package org.hfeng.leet.longestpalindromicsubstr;
 
 public class Solution {
+    private String addHelper(String s) {
+        StringBuilder sb = new StringBuilder("^");
+        // Extra #!!
+        sb.append("#");
+        for (int i = 0; i < s.length() ; i++) {
+            sb.append(s.charAt(i));
+            sb.append("#");
+        }
+        sb.append("$");
+        return sb.toString();
+    }
+
     public String longestPalindrome(String s) {
         if (s.length() == 0 || s == null) {
             return "";
         }
-        String processStr = handleStr(s);
+//        s has at least one number
+        String ns = addHelper(s);
+        int[] sizes = new int[ns.length()];
+        sizes[0] = 0;
+        sizes[ns.length() - 1] = 0;
+        int maxCenter = 0;
+        int maxRight = 0;
 
-        int[] value = new int[processStr.length()];
-
-        int maxIndex = 0;
-        int maxPos = 0;
-        for (int i = 1; i < value.length - 1; i++) {
-            int startSize = 0;
-            if (i < maxPos) {
-                int mirrorIndex = 2 * maxIndex - i;
-                startSize = Math.min(value[mirrorIndex], maxPos - i);
+        for (int i = 1; i < ns.length() - 1; i++) {
+            int len = 0;
+            if (i < maxRight) {
+                len = Integer.min(maxRight - i, sizes[2 * maxCenter - i]);
             }
-            while (processStr.charAt(i + startSize + 1) == processStr.charAt(i - startSize - 1)) {
-                startSize++;
+            while (ns.charAt(i - len - 1) == ns.charAt(i + len + 1)) {
+                ++len;
             }
-
-            value[i] = startSize;
-            if (startSize + i > maxPos) {
-                maxPos = startSize + i;
-                maxIndex = i;
-            }
-        }
-
-        int maxV = 0;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i] > value[maxV]) {
-                maxV = i;
+            sizes[i] = len;
+            if (i + sizes[i] > maxRight) {
+                maxRight = i + sizes[i];
+                maxCenter = i;
             }
         }
 
+        int maxSizeIndex = 0;
+        for (int i = 1; i < sizes.length; i++) {
+            if (sizes[i] > sizes[maxSizeIndex]) {
+                maxSizeIndex = i;
+            }
+        }
         StringBuilder ret = new StringBuilder();
-        for (int i = maxV - value[maxV]; i <= maxV + value[maxV]; i++) {
-            char now = processStr.charAt(i);
-
-            if (now != '^' && now != '$' && now != '#') {
-                ret.append(now);
+        for (int i = maxSizeIndex - sizes[maxSizeIndex]; i <= maxSizeIndex + sizes[maxSizeIndex]; i++) {
+            if (ns.charAt(i) != '^' && ns.charAt(i) != '$' && ns.charAt(i) != '#') {
+                ret.append(ns.charAt(i));
             }
         }
         return ret.toString();
-    }
-
-    private String handleStr(String s) {
-        StringBuilder sb = new StringBuilder("^#");
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-            sb.append('#');
-        }
-        sb.append('$');
-        return sb.toString();
     }
 }
